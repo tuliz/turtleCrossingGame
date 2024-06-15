a15965ca96a7ac96e21a9c5e4a2a9677a2288741
yarden <yarden.tulchinsky@gmail.com>
2024-06-15 20:00:40 +0300

    after player passes safely increase the speed for the cars. tweaking the cars the how they are showing

--- car.py
+++ car.py
@@ -8,17 +8,18 @@ class Car(Turtle):
     def create_car(self):
         self.color(random.choice(COLORS))
         self.penup()
+        self.car_speed = 5
         self.shape('square')
         self.speed('fastest')
         self.setheading(180)
         self.shapesize(stretch_wid=1,stretch_len=3)
-        self.goto(random.randint(350, 450), random.randint(-250, 280))
+        self.goto(random.randint(350, 800), random.randint(-250, 280))
 
 
     def move(self):
-        self.forward(5)
+        self.forward(self.car_speed)
 
     def random_place(self):
-        self.goto(random.randint(350, 450), random.randint(-250, 280))
+        self.goto(random.randint(350, 800), random.randint(-250, 280))
 
 
--- main.py
+++ main.py
@@ -3,34 +3,56 @@ from player import Player
 from scoreboard import Scoreboard
 from car import Car
 import time
+
+#screen settings
 screen = Screen()
 screen.screensize(600,600)
 screen.tracer(0)
+
 player = Player()
 score = Scoreboard()
+
+#listener for moving player forward
 screen.listen()
 screen.onkey(player.move_forward,'Up')
+
 screen.update()
+
 game_continue = True
 cars = []
 cap = 20
 
 while game_continue:
+
+    #if cap for cars still didnt reach
     if len(cars) < cap:
         new_car = Car()
         new_car.create_car()
         cars.append(new_car)
+
+
     for car in cars:
-        if player.distance(car) < 38:
+
+       #check if player is run over by car and end game if true
+        if player.distance(car) < 35:
             game_continue = False
             score.game_over()
+
+        #if car is passing the screen get it back from the other side
         if car.xcor() < -400:
             car.random_place()
         car.move()
     screen.update()
     time.sleep(0.1)
+
+    #check if player is passing the upper wall and get the score up
     if player.ycor() > 300:
         score.score_up()
+
+        #after getting to the next round up the cars speed
+        for car in cars:
+            car.car_speed += 5
+
         player.goto_start()
 
 screen.exitonclick()
